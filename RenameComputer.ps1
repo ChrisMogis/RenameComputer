f76310b64c027dcf7836b912e5f55bb06ec34acd
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2022-12-19 16:03:54 +0100

    Update RenameComputer.ps1

--- RenameComputer.ps1
+++ RenameComputer.ps1
@@ -1,31 +1,32 @@
-################################################################################################
-# This script is used to change computer name with Micrososft Intune for exemple.              #
-# Editor : Christopher Mogis                                                                   #
-# Date : 24/11/2022                                                                            #
-# Version 1.0 - Convert select action by drop down menu                                        #
-################################################################################################
+<#
+.DESCRIPTION
+This script is used to change computer name
 
-#Create Logs Directory
-New-Item -Force -Path "C:\Windows\Temp\Logs" -ItemType Directory
+.NOTES
+  Version:        1.0
+  Author:         Christopher Mogis
+  Creation Date:  24/11/2022
+#>
 
 #Variables
 $Date = Get-Date
 $Name = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
 $Serial = (Get-CimInstance win32_bios).serialnumber
 $Suffixe = "CCMT"
-$Logs = "C:\Windows\Temp\Logs\RenameComputer.log"
+$Logs = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\RenameComputer.log"
 
 #Computer Name 
-    Write-Output "$($Date) : Your computer name is $($Name)" | Tee-Object -FilePath $Logs -Append
+        Write-Output "$($Date) : Your computer name is $($Name)" | Tee-Object -FilePath $Logs -Append
 
 #Change Computer Name
 if ($Name -match "Desktop-") 
-{
-    Write-Output "$($Date) : The script change the computer name because it's not compliant with the naming convention" | Tee-Object -FilePath $Logs -Append
-    Rename-Computer -NewName "$($Suffixe)$($Serial)" -Force <# Action to perform if the condition is true #>
-    Write-Output "$($Date) : Computer is pending reboot" | Tee-Object -FilePath $Logs -Append
-}
+    {
+        Write-Output "$($Date) : The script change the computer name because it's not compliant with the naming convention" | Tee-Object -FilePath $Logs -Append
+        Rename-Computer -NewName "$($Suffixe)$($Serial)" -Force <# Action to perform if the condition is true #>
+        Write-Output "$($Date) : The new computer name is $($Suffixe)$($Serial)" | Tee-Object -FilePath $Logs -Append
+        Write-Output "$($Date) : Computer is pending reboot" | Tee-Object -FilePath $Logs -Append
+    }
 else
-{
-    Write-Output "$($Date) : The computer name $($Name) is compliant with the naming convention" | Tee-Object -FilePath $Logs -Append <# Action to perform if the condition is false #>
-}
+    {
+        Write-Output "$($Date) : The computer name $($Name) is compliant with the naming convention" | Tee-Object -FilePath $Logs -Append <# Action to perform if the condition is false #>
+    }
