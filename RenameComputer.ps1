07cdaf0623939031853f9b3e04bbe205c0c2538c
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2023-04-05 12:19:26 +0200

    Update RenameComputer.ps1

--- RenameComputer.ps1
+++ RenameComputer.ps1
@@ -11,7 +11,7 @@ This script is used to change computer name with Microsoft Intune
 #Variables
     $Date = Get-Date
     $CurrentName = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
-    $Suffixe = "W0010"
+    $Suffix = "W0010"
     $Logs = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\RenameComputer.log"
 
 #Delete old Logs files
@@ -24,19 +24,19 @@ This script is used to change computer name with Microsoft Intune
     $ComputerType = (Get-ComputerInfo).CsSystemFamily
     If($ComputerType -eq "Virtual Machine")
         {
-            Write-Output "$($Date) : Platform detection - $($ComputerType), using an automatically generated computer name." | Tee-Object -FilePath $Logs -Append
+            Write-Output "$($Date) : Platform detection - $($ComputerType) : using an automatically generated computer name." | Tee-Object -FilePath $Logs -Append
             $Serial = Get-Random
         }
     else
         {
-            Write-Output "$($Date) : Platform detection - $($ComputerType), using the service tag to generate the computer name." | Tee-Object -FilePath $Logs -Append
+            Write-Output "$($Date) : Platform detection - $($ComputerType) :  using the service tag to generate the computer name." | Tee-Object -FilePath $Logs -Append
             $Serial = (Get-CimInstance win32_bios).serialnumber
         }
 
 #Change Computer Name
     Write-Output "$($Date) : The script change the computer name because it's not compliant with the naming convention" | Tee-Object -FilePath $Logs -Append
-    Rename-Computer -NewName "$($Suffixe)$($Serial)" -Force
-    Write-Output "$($Date) : The new computer name is $($Suffixe)$($Serial)" | Tee-Object -FilePath $Logs -Append
+    Rename-Computer -NewName "$($Suffix)$($Serial)" -Force
+    Write-Output "$($Date) : The new computer name is $($Suffix)$($Serial)" | Tee-Object -FilePath $Logs -Append
         
 #Send message to user for request computer restart & reboot
     Shutdown -r -t 120 -f -c "Your administrator has implemented changes. To finalize the process, we will reboot your computer in 2 minutes."
